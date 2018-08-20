03ba5cbffc63e8ee7bb41c98a4daf9b1e37bd763
Ashwath Rabindranath <ashwath.rabindranath@gmail.com>
2018-08-19 23:02:53 -0400

    Update basicscraping.py

--- basicscraping.py
+++ basicscraping.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 
 from bs4 import BeautifulSoup
 
-def keyword_query(keyword):
+def key_query_indeed(keyword):
     """
 
     Takes Indeed parameters, converts, and returns it into appropriate query format.
@@ -21,6 +21,17 @@ def keyword_query(keyword):
 
     return url
 
+def key_query_ziprecruiter(keyword):
+
+    """
+    Takes Ziprecruiter parameters, converts, and returns it into appropriate query format.
+    """
+    url = "https://www.ziprecruiter.com/candidate/search?form=jobs-landing&search="
+    for words in keyword:
+        for word in words.split():
+            url+=word + '%20'
+        url = url + '&radius=5&location={}&page={}'
+    return url
 
 def extract_text(query):
     if query:
@@ -47,10 +58,11 @@ def get_summary_from_result(div):
 
 
 
-def extract_posts_to_df(keyword=[], city_set=[], max_results_per_city=int):
+
+def extract_indeed_posts_to_df(keyword=[], city_set=[], max_results_per_city=int):
 
 
-    url = keyword_query(keyword)
+    url = key_query_indeed(keyword)
 
     job_post = defaultdict(list)
     for city in city_set:
@@ -73,10 +85,37 @@ def extract_posts_to_df(keyword=[], city_set=[], max_results_per_city=int):
 
     return df
 
-def extract_posts_to_csv(keyword=[], city_set=[], max_results_per_city=int, file=0):
-"""Extracts city, state, company, summary and title from posts on indeed.
+def extract_ziprecruiter_posts_to_df(keyword=[], city_set=[], max_results_per_city=int):
+
+
+    url = key_query_ziprecruiter(keyword)
+
+    job_post = defaultdict(list)
+    for city in city_set:
+
+        for start in range(0, max_results_per_city, 10):
+            page = requests.get(url.format(city, start))
+            time.sleep(1)  #ensuring at least 1 second between page grabs
+            soup = BeautifulSoup(page.text, 'html.parser')
+
+            for div in soup.find_all(name='div', class_='job_content'):
+                job_post['city'].append(city)
+                job_post['title'].append(extract_text(div.find('span',class_='just_job_title')))
+                job_post['company'].append(extract_text(div.find('a',class_='t_org_link name')))
+                job_post['location'].append(extract_text(div.find('a',class_='t_location_link location')))
+                job_post['summary'].append(extract_text(div.find('p',class_='job_snippet')))
+
+    df = pd.DataFrame.from_records(job_post)
+    df.drop_duplicates(inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    return df
+
+
+def extract_indeed_posts_to_csv(keyword=[], city_set=[], max_results_per_city=int, file=0):
+    """Extracts city, state, company, summary and title from posts on indeed.
     Writes the posts data to a CSV file  """
-    url = keyword_query(keyword)
+    url = key_query_indeed(keyword)
 
     for city in city_set:
 
@@ -87,17 +126,45 @@ def extract_posts_to_csv(keyword=[], city_set=[], max_results_per_city=int, file
 
             for div in soup.find_all(name='div', attrs={'class':'row'}):
                 f.write(str(city)+',')
-                f.write(str(get_title_from_result(div))+',')
-                f.write(str(get_company_from_result(div))+',')
-                f.write(str(get_location_from_result(div))+',')
-                f.write(str(get_summary_from_result(div))+'\n')
+                f.write('\"'+str(get_title_from_result(div))+'\"'+',')
+                f.write('\"'+str(get_company_from_result(div))+'\"'+',')
+                f.write('\"'+str(get_location_from_result(div))+'\"'+',')
+                f.write('\"'+str(get_summary_from_result(div))+'\"'+'\n')
     return 0
 
+def extract_ziprecruiter_posts_to_csv(keyword=[], city_set=[], max_results_per_city=int, file = 0):
+
+
+    url = key_query_ziprecruiter(keyword)
+
+    job_post = defaultdict(list)
+    for city in city_set:
+
+        for start in range(0, max_results_per_city, 10):
+            page = requests.get(url.format(city, start))
+            time.sleep(1)  #ensuring at least 1 second between page grabs
+            soup = BeautifulSoup(page.text, 'html.parser')
+
+            for div in soup.find_all(name='div', class_='job_content'):
+                f.write(str(city)+",")
+                f.write('\"'+ str(extract_text(div.find('span',class_='just_job_title')))+'\"'+",")
+                f.write('\"'+str(extract_text(div.find('a',class_='t_org_link name')))+'\"'+",")
+                f.write('\"'+str(extract_text(div.find('a',class_='t_location_link location')))+'\"'+",")
+                f.write('\"'+str(extract_text(div.find('p',class_='job_snippet')))+'\"'+'\n')
+
+    df = pd.DataFrame.from_records(job_post)
+    df.drop_duplicates(inplace=True)
+    df.reset_index(drop=True, inplace=True)
+
+    return df
 
 
 cities = ['los angeles','chicago','san francisco','seattle','new york','austin',
           'philadelphia','atlanta','dallas','portland','phoenix','denver',
           'houston','miami','washington']
-f = open('posts.csv','w')
-extract_posts_to_csv(keyword=['data scientist'],city_set = cities, max_results_per_city = 1000, file = f)
+f = open('indeed_posts.csv','w')
+extract_indeed_posts_to_csv(keyword=['data scientist'],city_set = cities, max_results_per_city = 1000, file = f)
 f.close()
+g = open('ziprecruiter_posts.csv','w')
+extract_ziprecruiter_posts_to_csv(keyword=['data scientist'],city_set = cities, max_results_per_city = 1000, file = g)
+g.close()
